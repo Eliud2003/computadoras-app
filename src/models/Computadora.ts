327d5c573e934212100b4d356bfb3631a9ea3339
ELIUD ALFONSO SUAREZ CLEMENTE <eliud.suarezcle@ug.edu.ec>
2024-11-10 17:28:01 -0500

    avance

--- src/models/Computadora.ts
+++ src/models/Computadora.ts
@@ -3,7 +3,7 @@ export interface Computadora{
     id?:number; // es opcional , porq cuando recien se crea aun no hay
     marca: string;
     modelo: string;
-    año: string;
+    fecha: string;
     isGamer: boolean;
     poster: string
     precio: number;
